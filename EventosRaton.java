e9853fad6563f427302915f67430de9d7b42348c
ikerorozco <ikerorozco167@aragon.unam.mx>
2022-05-06 22:34:49 -0500

    tarea ioh

---
+++ EventosRaton.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.iud;
+
+
+public class InterfacesdeUsuario2210 {
+
+    public static void main(String[] args) {
+  
+        Miventana w = new Miventana();
+        
+        
+        
+        
+        
+    }
+    
+}
